4290bbc06099df860d060ed0aa926e9c4ea032f3
Singapakula Vamshi <113580561+Singapakula@users.noreply.github.com>
2024-02-26 14:47:24 +0530

    Update index.js

--- src/screens/Example/index.js
+++ src/screens/Example/index.js
@@ -4,7 +4,8 @@ import { View,Text } from "react-native";
 const Example=()=>{
     return(
         <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-            <Text>Hey!{'\n'}You didn't have any Example</Text>
+            <Text>Hey!{'\n'}You didn't have any Example
+    </Text>
         </View>
     )
 }
